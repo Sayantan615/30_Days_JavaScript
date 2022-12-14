c52485e9f4b0174c2582fe528c1459b84386c4da
sayantan <sayantanchowdhury615@gmail.com>
2022-12-14 21:42:26 +0530

    Day three Done

--- 03 - CSS Variables/server.js
+++ 03 - CSS Variables/server.js
@@ -1,7 +1,11 @@
 const inputs = document.querySelectorAll(".controls input");
+// It is a object which contains all input elements with the controls class
+console.log(inputs);
 function handleUpdate() {
   // console.log(this.value);
+
   console.log(this.dataset);
+  // It is a object which contains all the data elements
   const suffix = this.dataset.sizing || " ";
   // console.log(this.name);
   document.documentElement.style.setProperty(
